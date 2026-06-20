f8b1dcb4cda83c60e7c1a56f9c962b98d20d835c
vgrbuilds <grvignesh.official@gmail.com>
2026-06-21 00:37:00 +0530

    Allow VITE_GITHUB_CLIENT_ID to be configured from environment variables

--- client/src/services/githubOauth.js
+++ client/src/services/githubOauth.js
@@ -3,7 +3,7 @@
  * Redirects the user to GitHub's authorization page.
  */
 export const githubOauth = () => {
-  const GITHUB_CLIENT_ID = 'Ov23libiQMbU9qeN3E7x';
+  const GITHUB_CLIENT_ID = import.meta.env.VITE_GITHUB_CLIENT_ID || 'Ov23libiQMbU9qeN3E7x';
   const REDIRECT_URI = window.location.origin; // Redirects back to the frontend homepage
   
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=read:user,user:email`;
